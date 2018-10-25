1da4c827334399d9c51e1faa07271b3c02e9046c
Jendrik Wenke <j.wenke@ostfalia.de>
2018-10-25 19:39:03 +0200

    change data format for question to use a list of answers

--- src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/model/Question.java
+++ src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/model/Question.java
@@ -2,12 +2,13 @@ package de.ostfalia.teamprojekt.wwm.wikidatatest.model;
 
 import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue;
 
-import java.util.Set;
+import java.util.List;
 
 public class Question {
 	
-	public String          questionText;
+	public String questionText;
 	public PropertyIdValue propertyAskedOn;
-	public Set<String>     answers;
+	// the right answer should be first
+	public List<String> answers;
 	
 }
