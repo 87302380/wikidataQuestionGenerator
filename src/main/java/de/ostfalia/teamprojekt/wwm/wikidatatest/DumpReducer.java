2d2d22cb5eb4b28b2a2943f441bffc0763e5a1cf
4sakura <4sakura@web.de>
2018-10-25 19:10:09 +0200

    fixed LanguageFilter and generalized the DumpReducer a bit

--- src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/DumpReducer.java
+++ src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/DumpReducer.java
@@ -1,17 +1,19 @@
 package de.ostfalia.teamprojekt.wwm.wikidatatest;
 
+import de.ostfalia.teamprojekt.wwm.wikidatatest.model.Question;
 import de.ostfalia.teamprojekt.wwm.wikidatatest.processors.LanguageFilter;
 import de.ostfalia.teamprojekt.wwm.wikidatatest.processors.PredicateItemFilter;
 import de.ostfalia.teamprojekt.wwm.wikidatatest.processors.StatementCleaner;
 import de.ostfalia.teamprojekt.wwm.wikidatatest.questions.PokemonTypeQuestion;
+import de.ostfalia.teamprojekt.wwm.wikidatatest.questions.QuestionType;
+import de.ostfalia.teamprojekt.wwm.wikidatatest.questions.SharedBordersQuestionType;
 import org.wikidata.wdtk.datamodel.interfaces.EntityDocumentProcessor;
+import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
 
 import java.io.IOException;
 
 public class DumpReducer implements AutoCloseable {
-	
-	private static final String OUTPUT_FILE_NAME = "pokemon.json";
-	private static final String INPUT_FILE_NAME  = "../wikidata-20181001-all.json.bz2";
+	private static final String INPUT_FILE_NAME  = "src/main/resources/wikidata-20181001-all.json.bz2";
 	
 	private final DumpReader reader;
 	private final DumpWriter writer;
@@ -21,12 +23,25 @@ public class DumpReducer implements AutoCloseable {
 	 * serialization, and opens the file that we want to write to.
 	 *
 	 * @throws IOException if there is a problem opening the output file
+	 * @param arg argument that was passed over from the caller and specifies the type of filtering
 	 */
-	private DumpReducer() throws IOException {
-		PokemonTypeQuestion q = new PokemonTypeQuestion();
-		
-		this.writer = new DumpWriter(OUTPUT_FILE_NAME);
-		
+	private DumpReducer(String arg) throws IOException {
+		QuestionType q;
+		String output_file_name;
+		switch (arg) {
+			case "pokemon":
+				q = new PokemonTypeQuestion();
+				output_file_name = "pokemon.json";
+				break;
+			case "borders":
+				q = new SharedBordersQuestionType();
+				output_file_name = "borders.json";
+				break;
+			default:
+				throw new IllegalArgumentException("Bitte Argument übergeben!");
+		}
+
+		this.writer = new DumpWriter(output_file_name);
 		EntityDocumentProcessor processor =
 				new PredicateItemFilter(
 						new LanguageFilter(
@@ -46,7 +61,7 @@ public class DumpReducer implements AutoCloseable {
 	public static void main(String[] args) throws IOException {
 		IoHelpers.configureLogging();
 		
-		try (DumpReducer main = new DumpReducer()) {
+		try (DumpReducer main = new DumpReducer(args[0])) {
 			main.start();
 		}
 	}
