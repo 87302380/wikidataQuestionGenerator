af048184b243aa15c930c6c634b3c16b6004c0e2
Jendrik Wenke <j.wenke@ostfalia.de>
2018-10-26 20:20:40 +0200

    remove unused method from dump writer

--- src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/DumpWriter.java
+++ src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/DumpWriter.java
@@ -11,34 +11,30 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class DumpWriter implements EntityDocumentProcessor, AutoCloseable {
-	
+
 	private final JsonSerializer jsonSerializer;
 	private final String filename;
-	
+
 	public DumpWriter(final String filename) throws IOException {
 		OutputStream out = new BufferedOutputStream(IoHelpers.openExampleFileOuputStream(filename));
 		this.filename = filename;
 		this.jsonSerializer = new JsonSerializer(out);
 		this.jsonSerializer.open();
 	}
-	
-	public void serialize(ItemDocument itemDocument) {
-		jsonSerializer.processItemDocument(itemDocument);
-	}
-	
+
 	@Override public void close() {
 		System.out.println("Serialized " + this.jsonSerializer.getEntityDocumentCount() + " item documents to JSON file " + filename + ".");
 		this.jsonSerializer.close();
 	}
-	
+
 	@Override public void processItemDocument(final ItemDocument itemDocument) {
 		jsonSerializer.processItemDocument(itemDocument);
 	}
-	
+
 	@Override public void processPropertyDocument(final PropertyDocument propertyDocument) {
 		jsonSerializer.processPropertyDocument(propertyDocument);
 	}
-	
+
 	@Override public void processLexemeDocument(final LexemeDocument lexemeDocument) {
 		jsonSerializer.processLexemeDocument(lexemeDocument);
 	}
