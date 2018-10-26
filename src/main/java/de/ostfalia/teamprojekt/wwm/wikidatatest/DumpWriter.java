040fbb92800fe8f9dffac2e110cd6d7661411f5e
Jendrik Wenke <j.wenke@ostfalia.de>
2018-10-26 20:36:31 +0200

    error if the output file already exists and refactoring

--- src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/DumpWriter.java
+++ src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/DumpWriter.java
@@ -7,19 +7,72 @@ import org.wikidata.wdtk.datamodel.interfaces.LexemeDocument;
 import org.wikidata.wdtk.datamodel.interfaces.PropertyDocument;
 
 import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class DumpWriter implements EntityDocumentProcessor, AutoCloseable {
+
+	/**
+	 * The directory where to place files created by the example applications.
+	 */
+	private static final String EXAMPLE_OUTPUT_DIRECTORY = "results";
 
 	private final JsonSerializer jsonSerializer;
 	private final String filename;
 
 	public DumpWriter(final String filename) throws IOException {
-		OutputStream out = new BufferedOutputStream(IoHelpers.openExampleFileOuputStream(filename));
+		OutputStream out = new BufferedOutputStream(openExampleFileOuputStream(filename));
 		this.filename = filename;
 		this.jsonSerializer = new JsonSerializer(out);
 		this.jsonSerializer.open();
+	}
+
+	/**
+	 * Opens a new FileOutputStream for a file of the given name in the example
+	 * output directory ({@link DumpWriter#EXAMPLE_OUTPUT_DIRECTORY}). Any
+	 * file of this name that exists already will be replaced. The caller is
+	 * responsible for eventually closing the stream.
+	 *
+	 * @param filename
+	 *            the name of the file to write to
+	 * @return FileOutputStream for the file
+	 * @throws IOException
+	 *             if the file or example output directory could not be created
+	 */
+	static FileOutputStream openExampleFileOuputStream(String filename) throws IOException {
+		Path directoryPath = Paths.get(EXAMPLE_OUTPUT_DIRECTORY);
+
+		createDirectory(directoryPath);
+		File filePath = directoryPath.resolve(filename).toFile();
+		if (filePath.exists()) {
+			throw new FileAlreadyExistsException(filePath.toString());
+		}
+		return new FileOutputStream(filePath);
+	}
+
+	/**
+	 * Create a directory at the given path if it does not exist yet.
+	 *
+	 * @param path
+	 *            the path to the directory
+	 * @throws IOException
+	 *             if it was not possible to create a directory at the given
+	 *             path
+	 */
+	private static void createDirectory(Path path) throws IOException {
+		try {
+			Files.createDirectory(path);
+		} catch (FileAlreadyExistsException e) {
+			if (!path.toFile().isDirectory()) {
+				throw e;
+			}
+		}
 	}
 
 	@Override public void close() {
--- src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/IoHelpers.java
+++ src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/IoHelpers.java
@@ -26,22 +26,10 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 class IoHelpers {
-	
+
 	private IoHelpers() {}
-	
-	/**
-	 * The directory where to place files created by the example applications.
-	 */
-	private static final String EXAMPLE_OUTPUT_DIRECTORY = "results";
-	
+
 	static void configureLogging() {
 		// Create the appender that will write log messages to the console.
 		ConsoleAppender consoleAppender = new ConsoleAppender();
@@ -50,48 +38,9 @@ class IoHelpers {
 		consoleAppender.setLayout(new PatternLayout("%d{yyyy-MM-dd HH:mm:ss} %c{1}:%L %-5p - %m%n"));
 		// Change to Level.ERROR for fewer messages:
 		consoleAppender.setThreshold(Level.INFO);
-		
+
 		consoleAppender.activateOptions();
 		Logger.getRootLogger().addAppender(consoleAppender);
 	}
-	
-	/**
-	 * Opens a new FileOutputStream for a file of the given name in the example
-	 * output directory ({@link IoHelpers#EXAMPLE_OUTPUT_DIRECTORY}). Any
-	 * file of this name that exists already will be replaced. The caller is
-	 * responsible for eventually closing the stream.
-	 *
-	 * @param filename
-	 *            the name of the file to write to
-	 * @return FileOutputStream for the file
-	 * @throws IOException
-	 *             if the file or example output directory could not be created
-	 */
-	static FileOutputStream openExampleFileOuputStream(String filename) throws IOException {
-		Path directoryPath = Paths.get(EXAMPLE_OUTPUT_DIRECTORY);
-		
-		createDirectory(directoryPath);
-		Path filePath = directoryPath.resolve(filename);
-		return new FileOutputStream(filePath.toFile());
-	}
-	
-	
-	/**
-	 * Create a directory at the given path if it does not exist yet.
-	 *
-	 * @param path
-	 *            the path to the directory
-	 * @throws IOException
-	 *             if it was not possible to create a directory at the given
-	 *             path
-	 */
-	private static void createDirectory(Path path) throws IOException {
-		try {
-			Files.createDirectory(path);
-		} catch (FileAlreadyExistsException e) {
-			if (!path.toFile().isDirectory()) {
-				throw e;
-			}
-		}
-	}
+
 }
