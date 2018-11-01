9f029018c5a52992586778d9c1ac0040710569e6
Jendrik Wenke <j.wenke@ostfalia.de>
2018-11-01 15:10:50 +0100

    implement pokemon questions

--- src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/model/Question.java
+++ src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/model/Question.java
@@ -1,14 +1,22 @@
 package de.ostfalia.teamprojekt.wwm.wikidatatest.model;
 
-import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue;
-
-import java.util.List;
+import com.google.common.collect.ImmutableList;
 
 public class Question {
-	
-	public String questionText;
-	public PropertyIdValue propertyAskedOn;
+
+	public final String questionText;
 	// the right answer should be first
-	public List<String> answers;
-	
+	public final ImmutableList<String> answers;
+
+	public Question(final String questionText, final ImmutableList<String> answers) {
+		if (answers.size() != 4) {
+			throw new IllegalArgumentException("A question requires 4 answers, " + answers.size() + " given");
+		}
+		this.questionText = questionText;
+		this.answers = ImmutableList.copyOf(answers);
+	}
+
+	@Override public String toString() {
+		return String.format("%s 1) %s, 2) %s, 3) %s, 4) %s", questionText, answers.get(0), answers.get(1), answers.get(2), answers.get(3));
+	}
 }
--- src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/questions/PokemonTypeQuestion.java
+++ src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/questions/PokemonTypeQuestion.java
@@ -1,8 +1,11 @@
 package de.ostfalia.teamprojekt.wwm.wikidatatest.questions;
 
+import com.google.common.collect.ImmutableList;
 import de.ostfalia.teamprojekt.wwm.wikidatatest.model.Question;
+import org.apache.commons.compress.utils.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.wikidata.wdtk.datamodel.implementation.ItemIdValueImpl;
 import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
 import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.Statement;
@@ -10,41 +13,78 @@ import org.wikidata.wdtk.datamodel.interfaces.StatementGroup;
 import org.wikidata.wdtk.datamodel.interfaces.Value;
 import org.wikidata.wdtk.datamodel.interfaces.ValueSnak;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.Spliterators;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.toList;
 
 public class PokemonTypeQuestion implements QuestionType {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(PokemonTypeQuestion.class);
-	private static Map<String, Set<String>> pokemonByType = new HashMap<>();
+	private static final String PROPERTY_INSTANCE_OF = "P31";
+	private static final String PROPERTY_FROM_FICTIONAL_UNIVERSE = "P1080";
+	private static final String PROPERTY_DIFFERENT_FROM = "P1889";
+	private static final String PROPERTY_BULBAPEDIA_ARTICLE = "P4845";
+	private static final Set<String> IGNORE_STATEMENTS_WHEN_COUNTING = Sets.newHashSet(
+			PROPERTY_FROM_FICTIONAL_UNIVERSE, PROPERTY_DIFFERENT_FROM, PROPERTY_BULBAPEDIA_ARTICLE
+	);
+	private static final Comparator<ItemDocument> STATEMENT_COUNT_COMPARATOR = Comparator.comparingInt(PokemonTypeQuestion::numberOfKnowledgeStatements);
+	private static final Random RANDOM = new Random();
+	private static final String WIKIDATA_SITE_URL = "http://www.wikidata.org/entity/";
+	private static final int ESTIMATED_NUMBER_OF_WELL_KNOWN_POKEMON_PER_TYPE = 50;
+
+	private Map<String, List<ItemDocument>> pokemonByType = new HashMap<>();
+	private Map<String, String> typeLabels = new HashMap<>();
 
 	public PokemonTypeQuestion() {
-		if (pokemonByType.isEmpty()) {
-			try (Scanner s = new Scanner(getClass().getClassLoader().getResourceAsStream("pokemontypes.csv"), "UTF-8")) {
-				s.useDelimiter(",");
-				s.nextLine(); // skip first line
-				while (s.hasNext()) {
-					pokemonByType.put(s.next(), new HashSet<>(1000));
-					s.nextLine();
-				}
+		try (Scanner s = new Scanner(getClass().getClassLoader().getResourceAsStream("pokemontypes.csv"), "UTF-8")) {
+			s.useDelimiter("[,\n]");
+			s.nextLine(); // skip first line
+			while (s.hasNext()) {
+				String typeId = s.next();
+				String label = s.next();
+				pokemonByType.put(typeId, new ArrayList<>());
+				typeLabels.put(typeId, label);
 			}
 		}
 	}
 
+	private static int numberOfKnowledgeStatements(final ItemDocument itemDocument) {
+		int count = 0;
+		Iterator<Statement> it = itemDocument.getAllStatements();
+		while (it.hasNext()) {
+			Statement s = it.next();
+			if (!IGNORE_STATEMENTS_WHEN_COUNTING.contains(s.getStatementId())) {
+				count++;
+			}
+		}
+		return count;
+	}
+
 	@Override public boolean itemRelevant(final ItemDocument itemDocument) {
 		for (StatementGroup sg : itemDocument.getStatementGroups()) {
-			if (sg.getProperty().getId().equals("P31")) {
+			if (sg.getProperty().getId().equals(PROPERTY_INSTANCE_OF)) {
 				for (Statement s : sg.getStatements()) {
 					if (s.getClaim().getMainSnak() instanceof ValueSnak) {
 						Value v = ((ValueSnak) s.getClaim().getMainSnak()).getValue();
 						if (v instanceof ItemIdValue && pokemonByType.containsKey(((ItemIdValue) v).getId())) {
 							// german label might not exist
 							//LOGGER.log(Level.INFO, itemDocument.getLabels().get("de").getText() + ": " + ((ItemIdValue) v).getId());
-							LOGGER.info(itemDocument.getEntityId().getId() + " is of type " + ((ItemIdValue) v).getId());
+							LOGGER.info("{} is of type {}", itemDocument.getEntityId().getId(), ((ItemIdValue) v).getId());
 							return true;
 						}
 					}
@@ -55,7 +95,48 @@ public class PokemonTypeQuestion implements QuestionType {
 	}
 
 	@Override public Stream<Question> generateQuestions() {
-		return Stream.empty();
+		for (final List<ItemDocument> pokemon : pokemonByType.values()) {
+			pokemon.sort(STATEMENT_COUNT_COMPARATOR.reversed());
+		}
+
+
+		List<ItemDocument> wellKnownPokemon = pokemonByType.values()
+				.stream()
+				.map(Collection::stream)
+				.map(x -> x.limit(ESTIMATED_NUMBER_OF_WELL_KNOWN_POKEMON_PER_TYPE))
+				.flatMap(Function.identity())
+				.distinct()
+				.collect(toList());
+
+		Iterator<Question> questions = new Iterator<Question>() {
+			private Iterator<Entry<String, List<ItemDocument>>> pokemonByTypeIterator = PokemonTypeQuestion.this.pokemonByType.entrySet().iterator();
+
+			@Override public boolean hasNext() {
+				return pokemonByTypeIterator.hasNext();
+			}
+
+			@Override public Question next() {
+				Entry<String, List<ItemDocument>> entry = pokemonByTypeIterator.next();
+				String type = entry.getKey();
+				List<ItemDocument> pokemon = entry.getValue();
+				String text = "Welches dieser Pokemon ist ein " + typeLabels.get(type) + "?";
+				String correctPokemon = pokemon.get(Math.min(pokemon.size(), RANDOM.nextInt(ESTIMATED_NUMBER_OF_WELL_KNOWN_POKEMON_PER_TYPE))).findLabel("de");
+
+				Predicate<ItemDocument> pokemonHasType = i -> i.hasStatementValue(PROPERTY_INSTANCE_OF, new ItemIdValueImpl(type, WIKIDATA_SITE_URL));
+
+				List<String> wrongPokemon = RANDOM.ints(0, wellKnownPokemon.size())
+						.mapToObj(wellKnownPokemon::get)
+						.filter(pokemonHasType.negate())
+						.limit(3)
+						.map(i -> i.findLabel("de"))
+						.collect(toList());
+
+				ImmutableList<String> answers = ImmutableList.<String>builder().add(correctPokemon).addAll(wrongPokemon).build();
+				return new Question(text, answers);
+			}
+		};
+
+		return StreamSupport.stream(Spliterators.spliteratorUnknownSize(questions, 0), false);
 	}
 
 	@Override public void processItemDocument(final ItemDocument itemDocument) {
@@ -66,8 +147,12 @@ public class PokemonTypeQuestion implements QuestionType {
 						Value v = ((ValueSnak) s.getClaim().getMainSnak()).getValue();
 						if (v instanceof ItemIdValue) {
 							String pokemonTypeId = ((ItemIdValue) v).getId();
-							pokemonByType.get(pokemonTypeId).add(itemDocument.getLabels().get("de").getText());
-							LOGGER.info(itemDocument.getLabels().get("de").getText() + ": " + pokemonTypeId);
+							List<ItemDocument> pokemonWithThisType = pokemonByType.get(pokemonTypeId);
+							if (pokemonWithThisType != null) {
+								// instance of something that is a subclass of pokemon
+								LOGGER.info("{}: {}", itemDocument.getLabels().get("de").getText(), pokemonTypeId);
+								pokemonByType.get(pokemonTypeId).add(itemDocument);
+							}
 						}
 					}
 				}
