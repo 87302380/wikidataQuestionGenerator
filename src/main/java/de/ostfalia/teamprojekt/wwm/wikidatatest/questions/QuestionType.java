6eb1c188f1a3589777ac4b4f48592a3204dadc47
Lei Chen <le.chen@ostfalia.de>
2018-11-04 16:36:46 +0100

    Complete fairy tale characters

--- src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/questions/QuestionType.java
+++ src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/questions/QuestionType.java
@@ -4,11 +4,12 @@ import de.ostfalia.teamprojekt.wwm.wikidatatest.model.Question;
 import org.wikidata.wdtk.datamodel.interfaces.EntityDocumentProcessor;
 import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
 
+import java.io.IOException;
 import java.util.stream.Stream;
 
 public interface QuestionType extends EntityDocumentProcessor {
 
 	boolean itemRelevant(ItemDocument itemDocument);
 
-	Stream<Question> generateQuestions();
+	Stream<Question> generateQuestions() throws IOException;
 }
