ed6410a7b9878a1fc18ce45de358228460560714
Jendrik Wenke <j.wenke@ostfalia.de>
2018-11-12 11:25:40 +0100

    border question working

--- src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/questions/SharedBordersQuestion.java
+++ src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/questions/SharedBordersQuestion.java
@@ -1,13 +1,25 @@
 package de.ostfalia.teamprojekt.wwm.wikidatatest.questions;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterators;
 import de.ostfalia.teamprojekt.wwm.wikidatatest.model.Question;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.map.DefaultedMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.wikidata.wdtk.datamodel.interfaces.*;
+import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
+import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
+import org.wikidata.wdtk.datamodel.interfaces.Statement;
+import org.wikidata.wdtk.datamodel.interfaces.StatementGroup;
+import org.wikidata.wdtk.datamodel.interfaces.Value;
+import org.wikidata.wdtk.datamodel.interfaces.ValueSnak;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
@@ -16,41 +28,13 @@ import static java.util.stream.Collectors.toList;
 
 public class SharedBordersQuestion implements QuestionType {
 
-	class Country {
-		// Wird gesetzt, sobald Nachbarn gefunden werden
-		ItemIdValue id;
-		Set<ItemIdValue> adjacentCountries = new HashSet<>();
-
-		String name;
-
-		@Override
-		public boolean equals(Object o) {
-			if (this == o) return true;
-			if (o == null || getClass() != o.getClass()) return false;
-			Country country = (Country) o;
-			return Objects.equals(id, country.id);
-		}
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(id);
-		}
-	}
-
 	private static final Logger LOGGER = LoggerFactory.getLogger(SharedBordersQuestion.class);
-	private static final String PROPERTY_CONTINENT = "P30";
 	private static final String PROPERTY_INSTANCE_OF = "P31";
 	private static final String PROPERTY_DISSOLVED_OR_ABOLISHED = "P576";
 	private static final String PROPERTY_COUNTRY = "Q6256";
 	private static final String PROPERTY_SHARES_BORDER = "P47";
 	private static final Random RANDOM = new Random();
-	private static final String WIKIDATA_SITE_URL = "http://www.wikidata.org/entity/";
-
-	private Set<Country> countries = new HashSet<>();
-
-	public SharedBordersQuestion() {
-
-	}
+	private final Set<Country> countries = new HashSet<>();
 
 	@Override
 	public boolean itemRelevant(ItemDocument itemDocument) {
@@ -77,70 +61,42 @@ public class SharedBordersQuestion implements QuestionType {
 	}
 
 	@Override
-	public void processItemDocument(ItemDocument itemDocument) {
-		for (StatementGroup sg : itemDocument.getStatementGroups()) {
-			if (sg.getProperty().getId().equals(PROPERTY_CONTINENT)) {
-				for (Statement s : sg.getStatements()) {
-					if (s.getClaim().getMainSnak() instanceof ValueSnak) {
-						Value v = ((ValueSnak) s.getClaim().getMainSnak()).getValue();
-						if (v instanceof ItemIdValue) {
-							ItemIdValue continentId = ((ItemIdValue) v);
-							LOGGER.info("{}: {}", itemDocument.getLabels().get("de").getText(), continentId);
-
-							Country currentlyWorkingOn = getCountryForId(continentId);
-							//currentlyWorkingOn.itemDocument = itemDocument;
-
-							//TODO: Warum verschwindet Name?
-							// Mit der aktuellen Impl müsste Finden von Relatives und das Bilden von Sätzen mit Nomen viel einfacher sein
-							currentlyWorkingOn.name = itemDocument.findLabel("de") + "+";
-
-							Set<ItemIdValue> neighbours = getNeighbours(itemDocument);
-							for (ItemIdValue neighbour : neighbours) {
-								Country countryForId = getCountryForId(neighbour);
-								countryForId.adjacentCountries.add(continentId);
-							}
-						}
-					}
-				}
+	public Stream<Question> generateQuestions() {
+		// not in the dump, but neighbor of another country (eg greenland, soviet union)
+		Set<ItemIdValue> invalidCountryIds = new HashSet<>();
+		for (Iterator<Country> iterator = countries.iterator(); iterator.hasNext(); ) {
+			final Country country = iterator.next();
+			if (country.itemDocument == null) {
+				iterator.remove();
+				invalidCountryIds.add(country.id);
 			}
 		}
-	}
-
-	private Country getCountryForId(ItemIdValue id) {
-		Optional<Country> country = countries.stream().filter(c -> Objects.equals(c.id, id)).findFirst();
-		if(country.isPresent()) {
-			// Dieses Country wurde schon mal gesehen (entweder als Item direkt, oder als Relationship)
-			return country.get();
-		} else {
-			// Dieses Country wurde noch nie zuvor gesehen, wird angelegt
-			Country c = new Country();
-			c.id = id;
-			countries.add(c);
-			return c;
+		countries.forEach(c -> c.adjacentCountries.removeIf(invalidCountryIds::contains));
+		for (Country country : countries) {
+			calculateTransitiveNeighbors(country);
 		}
+		return Stream.generate(new BorderQuestionSupplier(countries));
 	}
 
-	private void calculateTransitiveNeighbors() {
-		/*
-		// Für jedes Land...
-		for (Map.Entry<ItemDocument, List<String>> item : this.neighbours.entrySet()) {
-			Set<String> transitiveNeighbors = new HashSet<>();
-
-			// Für jeden Nachbar des Lands...
-			List<String> idOfMyNeighbors = item.getValue();
-			for (String idOfMyNeighbor : idOfMyNeighbors) {
-				// ... werden die Nachbarn errechnet (Als die Nachbarn meiner Nachbarn)
-				ItemDocument neighbor = getNeighborForId(idOfMyNeighbor);
-				transitiveNeighbors.addAll(getNeighbours(neighbor));
+	private void calculateTransitiveNeighbors(Country c) {
+		for (ItemIdValue neighborId : c.adjacentCountries) {
+			Optional<Country> neighbor = getCountryForId(neighborId);
+			if (!neighbor.isPresent()) {
+				// just created, is invalid
+				continue;
 			}
-
-			// Hinweis: Die Nachbarn meiner Nachbarn beinhalten auf jeden Fall mich selbst.
-			transitiveNeighbours.put(item.getKey(), new ArrayList<>(transitiveNeighbors));
+			Set<ItemIdValue> neighbourNeighbours = getNeighbours(neighbor.get().itemDocument);
+			neighbourNeighbours.removeIf(c.adjacentCountries::contains);
+			c.transitiveNeighbors.addAll(neighbourNeighbours);
 		}
-		*/
+		c.transitiveNeighbors.removeIf(n -> n.equals(c.id));
 	}
 
-	private Set<ItemIdValue> getNeighbours(ItemDocument country) {
+	private Optional<Country> getCountryForId(final ItemIdValue id) {
+		return countries.stream().filter(c -> Objects.equals(c.id, id)).findFirst();
+	}
+
+	private static Set<ItemIdValue> getNeighbours(ItemDocument country) {
 		Set<ItemIdValue> neighbours = new HashSet<>();
 		for (StatementGroup sg : country.getStatementGroups()) {
 			if (sg.getProperty().getId().equals(PROPERTY_SHARES_BORDER)) {
@@ -148,7 +104,7 @@ public class SharedBordersQuestion implements QuestionType {
 					if (s.getClaim().getMainSnak() instanceof ValueSnak) {
 						Value v = ((ValueSnak) s.getClaim().getMainSnak()).getValue();
 						if (v instanceof ItemIdValue) {
-							neighbours.add((ItemIdValue)v);
+							neighbours.add((ItemIdValue) v);
 						}
 					}
 				}
@@ -158,41 +114,124 @@ public class SharedBordersQuestion implements QuestionType {
 	}
 
 	@Override
-	public Stream<Question> generateQuestions() {
+	public void processItemDocument(ItemDocument itemDocument) {
+		for (StatementGroup sg : itemDocument.getStatementGroups()) {
+			if (sg.getProperty().getId().equals(PROPERTY_INSTANCE_OF)) {
+				for (Statement s : sg.getStatements()) {
+					if (s.getClaim().getMainSnak() instanceof ValueSnak) {
+						Value v = ((ValueSnak) s.getClaim().getMainSnak()).getValue();
+						if (v instanceof ItemIdValue) {
+							ItemIdValue value = ((ItemIdValue) v);
+							if (!value.getId().equals(PROPERTY_COUNTRY)) {
+								continue;
+							}
+							LOGGER.info("{}: {}", itemDocument.getLabels().get("de").getText(), value);
 
-		calculateTransitiveNeighbors();
-		return Stream.generate(new BorderQuestionSupplier());
+							Country currentlyWorkingOn = createOrGetCountryForId(itemDocument.getEntityId());
+
+							//TODO: Warum verschwindet Name?
+							// Mit der aktuellen Impl müsste Finden von Relatives und das Bilden von Sätzen mit Nomen viel einfacher sein
+							currentlyWorkingOn.name = itemDocument.findLabel("de");
+							currentlyWorkingOn.itemDocument = itemDocument;
+
+							Set<ItemIdValue> neighbours = getNeighbours(itemDocument);
+							currentlyWorkingOn.adjacentCountries.addAll(neighbours);
+							for (ItemIdValue neighbour : neighbours) {
+								Country countryForId = createOrGetCountryForId(neighbour);
+								countryForId.adjacentCountries.add(itemDocument.getEntityId());
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
-	private class BorderQuestionSupplier implements Supplier<Question> {
-		private final List<ItemDocument> countries = null;
+	private Country createOrGetCountryForId(ItemIdValue id) {
+		Optional<Country> country = getCountryForId(id);
+		if (country.isPresent()) {
+			// Dieses Country wurde schon mal gesehen (entweder als Item direkt, oder als Relationship)
+			LOGGER.info("found country {}", id);
+			return country.get();
+		} else {
+			// Dieses Country wurde noch nie zuvor gesehen, wird angelegt
+			LOGGER.info("creating country {}", id);
+			Country c = new Country();
+			c.id = id;
+			countries.add(c);
+			return c;
+		}
+	}
 
-		public BorderQuestionSupplier() {
-			//countries = countriesByContinent.values().stream().flatMap(Collection::stream).collect(toList());
+
+	private static class BorderQuestionSupplier implements Supplier<Question> {
+		private final List<Country> countries;
+
+		BorderQuestionSupplier(Set<Country> countries) {
+			this.countries = new ArrayList<>(countries);
 		}
 
 		@Override
 		public Question get() {
-			/*
-			ItemDocument type = countries.get(RANDOM.nextInt(countries.size()));
-			List<ItemDocument> country = countriesByContinent.get(type);
-			String text = "Welches dieser Länder grenzt an " + continentLabels.get(type) + "?";
-			String correctCountry = country.get(country.size() - 1).findLabel("de");
+			final Country country = getRandomCountry();
 
-			Predicate<ItemDocument> pokemonHasType = i -> i.hasStatementValue(PROPERTY_SHARES_BORDER , new ItemIdValueImpl(country, WIKIDATA_SITE_URL));
+			String correctAnswer = findCountryById(Iterators.get(country.adjacentCountries.iterator(), country.adjacentCountries.size() - 1)).orElseThrow(
+					IllegalStateException::new).name;
 
-			List<String> wrongCountries = RANDOM.ints(0, countries.size())
+			String text = "Welches dieser Länder grenzt an " + country.name + "?";
+
+			List<ItemIdValue> transitiveNeighbors = new ArrayList<>(country.transitiveNeighbors);
+
+			List<String> wrongAnswers = RANDOM.ints(0, transitiveNeighbors.size())
 					.distinct()
-					.mapToObj(countries::get)
-					.filter(pokemonHasType.negate())
+					.mapToObj(transitiveNeighbors::get)
+					.map(this::findCountryById)
+					.filter(Optional::isPresent)
+					.map(Optional::get)
 					.limit(3)
-					.map(i -> i.findLabel("de"))
+					.map(c -> c.name)
 					.collect(toList());
 
-			ImmutableList<String> answers = ImmutableList.<String>builder().add(correctCountry).addAll(wrongCountries).build();
+			ImmutableList<String> answers = ImmutableList.<String>builder().add(correctAnswer).addAll(wrongAnswers).build();
 			return new Question(text, answers);
-			*/
-			return null;
+		}
+
+		private Country getRandomCountry() {
+			Country tmp;
+			do {
+				tmp = countries.get(RANDOM.nextInt(countries.size()));
+			} while (tmp.adjacentCountries.isEmpty());
+			return tmp;
+		}
+
+		private Optional<Country> findCountryById(final ItemIdValue id) {
+			return countries.stream().filter(c -> c.id.equals(id)).findFirst();
+		}
+	}
+
+
+
+
+	private static class Country {
+		// Wird gesetzt, sobald Nachbarn gefunden werden
+		ItemDocument itemDocument;
+		ItemIdValue id;
+		Set<ItemIdValue> adjacentCountries = new HashSet<>();
+		Set<ItemIdValue> transitiveNeighbors = new HashSet<>();
+
+		String name;
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(id);
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			Country country = (Country) o;
+			return Objects.equals(id, country.id);
 		}
 	}
 }
