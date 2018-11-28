6dc8674823bfd6d2ec1173d40ebdddf9ec7aac5f
CMA <87302380@qq.com>
2018-11-28 03:22:59 +0100

    Rewrite fairy tales type question

--- src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/questions/FairyTaleCharacterQuestionType.java
+++ src/main/java/de/ostfalia/teamprojekt/wwm/wikidatatest/questions/FairyTaleCharacterQuestionType.java
@@ -9,8 +9,6 @@ import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
 import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.Statement;
 import org.wikidata.wdtk.datamodel.interfaces.StatementGroup;
-import org.wikidata.wdtk.datamodel.interfaces.Value;
-import org.wikidata.wdtk.datamodel.interfaces.ValueSnak;
 
 
 import java.util.*;
@@ -21,40 +19,37 @@ import static java.util.stream.Collectors.toList;
 public class FairyTaleCharacterQuestionType implements QuestionType {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(FairyTaleCharacterQuestionType.class);
-	private static final String PROPERTY_PRESENT_IN_WORK = "P1441";
+//	private static final String PROPERTY_PRESENT_IN_WORK = "P1441";
 	private static final String PROPERTY_GENRE = "P136";
-//	private static final String PROPERTY_CHARACTERS = "P674";
-//	private static final String FAIRY_TALE_PATH = "fairyTaleResources/fairyTales.csv";
+	private static final String PROPERTY_CHARACTERS = "P674";
+	private int counter = 0;
 
-	private static final Map<String, Set<String>> fairyTaleToCharacters = new HashMap<>();
-	private static final Map<String, Set<String>> charactersToFairyTale = new HashMap<>();
-	private static final Map<String, String> fairyTaleLabel = new HashMap<>();
+	private static final Map<String, Set<String>> workToCharacter = new HashMap<>();
+	private static final Map<String, String> workLabel = new HashMap<>();
+	private static final Map<String, String> charactersLabel = new HashMap<>();
+	private static final Set<String> work = new HashSet<>();
 	private static final Set<String> genreProperty = new HashSet<>();
 
 	public FairyTaleCharacterQuestionType() {
+
 		addGenreProperty("Q699");
 		addGenreProperty("Q7832362");
 		addGenreProperty("Q20730955");
 
-//		if (fairyTaleToCharacters.isEmpty()) {
-//			try (Scanner s = new Scanner(getClass().getClassLoader().getResourceAsStream(FAIRY_TALE_PATH), "UTF-8")) {
-//				s.useDelimiter("\n");
-//				while (s.hasNext()) {
-//					String[] fairyTale = s.next().split(",");
-//					fairyTaleToCharacters.put(fairyTale[0], new HashSet<>(1000));
-//					fairyTaleLabel.put(fairyTale[0],fairyTale[1]);
-//					s.nextLine();
-//				}
-//			}
-//		}
 	}
-
 	private void addGenreProperty(String fairyEntity){
-		genreProperty.add(fairyEntity);
+		genreProperty.add(fairyEntity);             //add need Work
 	}
 
-	@Override public boolean canGenerateQuestions() {
-		return true;
+
+	@Override
+	public void onStartDumpReading() {
+		counter++;
+	}
+
+	@Override
+	public boolean canGenerateQuestions() {
+		return counter == 2;
 	}
 
 	@Override public Stream<Question> generateQuestions() {
@@ -65,41 +60,45 @@ public class FairyTaleCharacterQuestionType implements QuestionType {
 	}
 
 	@Override public void processItemDocument(final ItemDocument itemDocument) {
-		for (StatementGroup sg : itemDocument.getStatementGroups()) {
-			if (sg.getProperty().getId().equals(PROPERTY_PRESENT_IN_WORK)) {
-				for (Statement s : sg.getStatements()) {
-					if (s.getClaim().getMainSnak() instanceof ValueSnak) {
-						Value v = ((ValueSnak) s.getClaim().getMainSnak()).getValue();
-						if (v instanceof ItemIdValue) {
-							String fairyTaleId = ((ItemIdValue) v).getId();
-							LOGGER.info("{} is a character in {}", itemDocument.getLabels().get("de").getText(), fairyTaleId);
-							if (charactersToFairyTale.containsKey(fairyTaleId)){
-								Set<String> workIn = charactersToFairyTale.get(fairyTaleId);
-								workIn.add(itemDocument.getEntityId().getId());
-							}else {
-								Set<String> workIn = new HashSet<>();
-								workIn.add(fairyTaleId);
-								charactersToFairyTale.put(itemDocument.getEntityId().getId(),workIn);
-								//	characters.add(itemDocument.getLabels().get("de").getText());
-							}
+		String itemId = itemDocument.getEntityId().getId();
+		if (counter == 2){
+			if (charactersLabel.containsKey(itemId)){
+				String characterName = itemDocument.getLabels().get("de").getText();
+				charactersLabel.put(itemId,characterName);
+			}
 
-						}
+		}else if (counter == 1){
+
+			boolean isNeedWork = false;
+
+			for (StatementGroup sg : itemDocument.getStatementGroups()) {
+				if (sg.getProperty().getId().equals(PROPERTY_GENRE)) {
+					for (Statement s : sg.getStatements()) {
+                        ItemIdValue itemIdValue = (ItemIdValue) s.getValue();
+                        if (itemIdValue!=null){
+                            String  genre = itemIdValue.getId();
+                            if (genreProperty.contains(genre)) {
+                                isNeedWork = true;
+                            }
+                        }
 					}
+
 				}
 			}
-			if (sg.getProperty().getId().equals(PROPERTY_GENRE)) {
-				for (Statement s : sg.getStatements()) {
-					if (s.getClaim().getMainSnak() instanceof ValueSnak) {
-						Value v = ((ValueSnak) s.getClaim().getMainSnak()).getValue();
-						if (v instanceof ItemIdValue) {
-							if (genreProperty.contains(((ItemIdValue) v).getId())){
-								fairyTaleToCharacters.put(s.getClaim().getSubject().getId()
-										, new HashSet<>(1000));
-								fairyTaleLabel.put(s.getClaim().getSubject().getId()
-										,itemDocument.getLabels().get("de").getText());
-							}
-						}
+			for (StatementGroup sg : itemDocument.getStatementGroups()){
+				if (sg.getProperty().getId().equals(PROPERTY_CHARACTERS)&&isNeedWork)	{
+
+					workLabel.put(itemDocument.getEntityId().getId(),itemDocument.getLabels().get("de").getText());
+
+					Set<String> characters = new HashSet<>();
+
+					for (Statement s : sg.getStatements()) {
+						String character = ((ItemIdValue)s.getValue()).getId();
+						characters.add(character);
+						charactersLabel.put(character,null);
 					}
+					workToCharacter.put(itemId,characters);
+
 				}
 			}
 		}
@@ -109,27 +108,6 @@ public class FairyTaleCharacterQuestionType implements QuestionType {
 
 		private static final Random RANDOM = new Random();
 
-		QuestionGenerator(){
-
-
-			for (String key : charactersToFairyTale.keySet()){
-				for (String workIn:charactersToFairyTale.get(key)){
-					if (fairyTaleToCharacters.containsKey(workIn)){
-						Set<String> characters = fairyTaleToCharacters.get(workIn);
-						characters.add(key);
-					}
-				}
-			}
-			ArrayList<String> emptyKeyValue = new ArrayList<>();
-			for (String key : fairyTaleToCharacters.keySet()){
-				if (fairyTaleToCharacters.get(key).isEmpty()){
-					emptyKeyValue.add(key);
-				}
-			}
-			for (String key : emptyKeyValue){
-				fairyTaleToCharacters.remove(key);
-			}
-		}
 
 //		/**
 //		 * Convert lines of a csv file to a map of all key-value-pairs.
@@ -154,17 +132,20 @@ public class FairyTaleCharacterQuestionType implements QuestionType {
 			String fairyTaleInQuestion = randomFairyTale();
 			List<String> answers = generateAnswers(fairyTaleInQuestion);
 
-	//		answers = idToLabel(answers, fairyTaleLabel);
+			answers = idToLabel(answers, workLabel);
 
-			Iterator character = fairyTaleToCharacters.get(fairyTaleInQuestion).iterator();
-			String text = character.next() + " kommt aus welchen folgenden Märchen?";
-
+			String character = getRandomElement(workToCharacter.get(fairyTaleInQuestion));
+			String text ;
+			if (charactersLabel.get(character)!=null){
+				text = charactersLabel.get(character) + " kommt aus welchen folgenden Märchen?";
+			}else {
+				text = character + " kommt aus welchen folgenden Märchen?";
+			}
 			return new Question(text, ImmutableList.copyOf(answers));
 		}
 
 		private String randomFairyTale() {
-
-			String[] FairyTale = fairyTaleToCharacters.keySet().toArray(new String[0]);
+			String[] FairyTale = workToCharacter.keySet().toArray(new String[0]);
 			return FairyTale[RANDOM.nextInt(FairyTale.length)];
 		}
 
@@ -189,6 +170,17 @@ public class FairyTaleCharacterQuestionType implements QuestionType {
 
 		private static List<String> idToLabel(List<String> list, Map<String, String> map) {
 			return list.stream().map(map::get).peek(e -> Objects.requireNonNull(e, "no label for " + e)).collect(toList());
+		}
+		private static <E> E getRandomElement(Set<E> set){
+			int rn = (int) (Math.random() * (set.size()));
+			int i = 0;
+			for (E e : set) {
+				if(i==rn){
+					return e;
+				}
+				i++;
+			}
+			return null;
 		}
 	}
 
